71b031f3c2f7c0a11118e76f4e2a937157d1277d
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-04-12 22:24:40 -0500

    moved inquirer code from shape.js to index.js

---
+++ index.js
@@ -0,0 +1,26 @@
+// Imports the inquirer package
+const inquirer = require('inquirer');
+
+// Prompts for required user input to generate SVG file
+inquirer
+    .createPromptModule([
+        {
+            name: 'text',
+            message: 'Enter up to three characters:'
+        },
+        {
+            name: 'textColor',
+            message: 'Enter text color keyword (OR a hexidecimal number):'
+        },
+        {
+            name: 'shape',
+            message: 'Select a shape from the following list:'
+        },
+        {
+            name: 'shapeColor',
+            message: 'Enter shape color keyword (OR a hexidecimal number):'
+        }
+    ])
+    .then(answers => {
+        console.log('Logo has been generated', answers); // Code to generate SVG logo goes here
+    })
