96ae2a78c591c8968c07a0934cf4610ab8e6f702
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-04-18 12:58:35 -0500

    updated comments

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// Imports CLI class constructor
 const CLI = require('./lib/cli');
 
+// Decalred variable cli to store value of instantiating a new CLI class
 const cli = new CLI();
+
+// Function expression
+const run = () => {};
+
+// Function called to run application
+run(cli);
