150c5980ef193379572464cbcaff7276181905f5
Jstanion <123262263+Jstanion@users.noreply.github.com>
2023-04-17 22:30:17 -0500

    removed instantiate tests and passed renderHTML tests

--- tests/circle.test.js
+++ tests/circle.test.js
@@ -1,20 +1,12 @@
 const Circle = require('../lib/circle');
 
 describe('Circle', () => {
-    describe('Instantiate', () => {
-        it('should create a new Circle Object', () => {
-            const shape = new Circle()
-
-            expect(shape).toBeDefined();
-        });
-
     describe('Values passed', () => {
         it('should return the passed color value to the rendered HTML string', () => {
             const shape = new Circle();
             shape.setColor('blue')
 
             expect(shape.render()).toEqual('<circle cx="150" cy="105" r="80" fill="blue"/>');
-            });
         });
     });
 });
--- tests/square.test.js
+++ tests/square.test.js
@@ -1,20 +1,12 @@
 const Square = require('../lib/square');
 
 describe('Square', () => {
-    describe('Instantiate', () => {
-        it('should create a new Square Object', () => {
-            const shape = new Square()
-
-            expect(shape).toBeDefined();
-        });
-
     describe('Values passed', () => {
         it('should return the passed color value to the rendered HTML string', () => {
             const shape = new Square();
             shape.setColor('orange')
 
             expect(shape.render()).toEqual('<rect x="75" y="30" width="150" height="150" fill="orange"/>');
-            });
         });
     });
 });
